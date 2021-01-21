558f82b0edd80c0cc32ee5dcf615eba6327b26c5
Sora <645265146@qq.com>
2021-01-21 11:28:39 +0800

    初始化项目

---
+++ src/main/java/com/dfrz/javaprojectstage3/javaprojectstage3/Javaprojectstage3Application.java
@@ -0,0 +1,13 @@
+package com.dfrz.javaprojectstage3.javaprojectstage3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Javaprojectstage3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Javaprojectstage3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/dfrz/javaprojectstage3/javaprojectstage3/Javaprojectstage3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dfrz.javaprojectstage3.javaprojectstage3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Javaprojectstage3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
