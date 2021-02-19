f79b89453d06c2172fc763da866b907b57c9f1ef
WenQi <1768094650@qq.com>
2021-02-19 22:26:46 +0800

    温奇

--- src/main/java/com/dfrz/javaprojectstage3/mapper/StepMapper.java
+++ src/main/java/com/dfrz/javaprojectstage3/mapper/StepMapper.java
@@ -1,10 +1,16 @@
 package com.dfrz.javaprojectstage3.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.dfrz.javaprojectstage3.bean.Petition;
 import com.dfrz.javaprojectstage3.bean.Step;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StepMapper extends BaseMapper<Step> {
+
+    public IPage<Petition> getPetitionPagebyCurrentuser(Page page, @Param("examineuser") Integer examineuser, @Param("userid") Integer userid);
 
 }
